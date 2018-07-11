ad25a16a7c70a8ee97016a8377c671ee129a11fe
felicia mtoka <feliciahmtoka@gmail.com>
2018-07-11 16:27:21 +0300

    commited changes

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,43 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
-    <form>
-    <textarea placeholder="Add new Task" v-model="newTask"></textarea>
-    <p>{{ newTask }}</p>
-    <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
-    </form>
-    <ul>
-    <li v-for="task in tasks">{{ task }}</li>
-    </ul>
+    <div v-for="article in news">
+      <img :src="article.urlToImage"  alt="">
+        
+      <h3>{{article.title}}</h3><br>
+      <h4>{{article.author}}</h4><br>
+      <p>{{article.description}}</p>
+      
+      <a class="button" :href="article.url">Read More</a>
+      
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ec9f40e635554fa2a6e35c55159ff123";
+const data = {
+news :[]
+}
 export default {
   data: function() {
-    return {
-      title: 'My Todo List',
-      newTask:'This is a sample',
-      tasks:[
-
-      ]
-    }
+    return data
+      
+  },
+  created() {
+    this.loadNews();
   },
   methods:{
-  	addTask(){
-  		
-  		this.tasks.unshift(this.newTask)
-  		this.newTask = '';
 
-  	}
+  loadNews(){
+  axios.get(url).then(function(response) {
+  console.log(response.data.atricles)
+  data.news = response.data.articles
+  }).catch(function(error) {
+  console.log(error.message)
+  })
+  }
+  	
   }
 }
 </script>
