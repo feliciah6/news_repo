92361f143b33e6b3ebdd893b43700eb57f674361
felicia mtoka <feliciahmtoka@gmail.com>
2018-07-11 15:06:00 +0300

    initial repository

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h3>{{ title }}</h3>
+    <form>
+    <textarea placeholder="Add new Task" v-model="newTask"></textarea>
+    <p>{{ newTask }}</p>
+    <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
+    </form>
+    <ul>
+    <li v-for="task in tasks">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'My Todo List',
+      newTask:'This is a sample',
+      tasks:[
+
+      ]
+    }
+  },
+  methods:{
+  	addTask(){
+  		
+  		this.tasks.unshift(this.newTask)
+  		this.newTask = '';
+
+  	}
+  }
+}
+</script>
